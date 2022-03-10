4a1cb11b1d163c47674f370c9e07acf8c192927e
Mtuart <stui.eichler02@gmail.com>
2022-03-10 19:28:27 +0100

    Don't drink and drive!

--- src/main/java/de/stuart/speedometer/ReloadCommand.java
+++ src/main/java/de/stuart/speedometer/ReloadCommand.java
@@ -7,6 +7,10 @@ import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.logging.Level;
+
+import static de.stuart.speedometer.Main.log;
+
 public class ReloadCommand implements CommandExecutor {
     
     private static Main plugin = Main.instance;
@@ -15,11 +19,13 @@ public class ReloadCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if(sender instanceof ConsoleCommandSender){
             plugin.reloadConfig();
+            log.log(Level.INFO, "Reloaded the config!");
         } else if (sender instanceof Player){
             Player player = (Player) sender;
             
             if(player.hasPermission("speedometer.reload")){
                 plugin.reloadConfig();
+                player.sendMessage("§8| §6Speedometer §7: Reloaded the config!");
             }
         }
         return true;
